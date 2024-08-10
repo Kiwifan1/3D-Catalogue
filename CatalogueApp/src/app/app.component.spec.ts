9ac40113110dc2f740dab41746f512435963e74a
Joshua Venable <70110252+Kiwifan1@users.noreply.github.com>
2024-08-10 12:04:18 -0700

    update init

--- CatalogueApp/src/app/app.component.spec.ts
+++ CatalogueApp/src/app/app.component.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'catalogue-app' title`, () => {
+  it(`should have as title 'CatalogueApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('catalogue-app');
+    expect(app.title).toEqual('CatalogueApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, catalogue-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, CatalogueApp');
   });
 });
